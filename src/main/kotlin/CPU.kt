8e4c4f06f9cfbb8a82e4b9fcd6e9630cac8a44c5
Benjamin <hickenlooperbe@gmail.com>
2025-08-03 12:04:39 -0600

    added memory elements to CPU

--- src/main/kotlin/CPU.kt
+++ src/main/kotlin/CPU.kt
@@ -1,5 +1,13 @@
+import memory.MemoryAdapter
+import memory.RAM
+import memory.ROM
+import memory.ScreenBuffer
+
 class CPU {
     val registers: CPURegisters = CPURegisters()
-    // todo
+    val RAM: MemoryAdapter = RAM()
+    val ROM: MemoryAdapter = ROM()
+    val SCREEN_BUFFER: MemoryAdapter = ScreenBuffer()
 
+    // todo
 }
