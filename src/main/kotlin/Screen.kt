eaed403c1e6c6c721ccb822a298230e12e7a52ea
Benjamin <hickenlooperbe@gmail.com>
2025-08-03 21:57:03 -0600

    changed screen chars to $

--- src/main/kotlin/Screen.kt
+++ src/main/kotlin/Screen.kt
@@ -6,7 +6,7 @@ class Screen: ScreenBufferObserver {
             for (j in 0..7) {
                 val byteValue = memory[i * 8 + j]
                 if (byteValue == 0.toUByte()) {
-                    print('0') // Display '0' for empty pixels
+                    print('$') // Display '0' for empty pixels
                 } else {
                     print(byteValue.toInt().toChar()) // Convert to ASCII character
                 }
