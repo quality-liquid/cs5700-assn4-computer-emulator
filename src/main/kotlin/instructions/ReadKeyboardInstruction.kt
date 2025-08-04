8bc4415004c4d9339b2ce6c69d6cdc436c0436e9
Benjamin <hickenlooperbe@gmail.com>
2025-08-03 18:35:50 -0600

    implemented read keyboard instruction

--- src/main/kotlin/instructions/ReadKeyboardInstruction.kt
+++ src/main/kotlin/instructions/ReadKeyboardInstruction.kt
@@ -1,15 +1,21 @@
 package instructions
 
 class ReadKeyboardInstruction: InstructionTemplate {
-    override fun parseNibbles(instruction: Int) {
-        TODO("Not yet implemented")
+    override fun parseNibbles(instruction: Int): Map<String, Int> {
+        val nibbles = mutableMapOf<String, Int>()
+        nibbles["dest"] = (instruction shr 8) and 0x0f
+        return nibbles
     }
 
-    override fun performOperation() {
-        TODO("Not yet implemented")
+    override fun performOperation(nibbles: Map<String, Int>, context: CPU.CPUContext) {
+        println("Enter up to one byte as one or two hex digits (0-f): ")
+        // todo: is this okay? blocking whole executor
+        val value: String? = readln()
+        val valueAsByte: UByte = value?.toUByte() ?: 0u
+        context.registerMap[nibbles["dest"]]?.value = valueAsByte
     }
 
-    override fun programCounter() {
-        TODO("Not yet implemented")
+    override fun programCounter(context: CPU.CPUContext) {
+        context.registers.P = (context.registers.P + 2u).toUShort()
     }
 }
