b95bb471c16671a137fdaf04696efb5d0c00cda7
Benjamin <hickenlooperbe@gmail.com>
2025-08-03 19:09:48 -0600

    implemented convert to base 10 instruction

--- src/main/kotlin/instructions/ConvertToBaseTenInstruction.kt
+++ src/main/kotlin/instructions/ConvertToBaseTenInstruction.kt
@@ -1,15 +1,22 @@
 package instructions
 
 class ConvertToBaseTenInstruction: InstructionTemplate {
-    override fun parseNibbles(instruction: Int) {
-        TODO("Not yet implemented")
+    override fun parseNibbles(instruction: Int): Map<String, Int> {
+        val nibbles = mutableMapOf<String, Int>()
+        nibbles["src"] = (instruction shr 8) and 0x0f
+        return nibbles
     }
 
-    override fun performOperation() {
-        TODO("Not yet implemented")
+    override fun performOperation(nibbles: Map<String, Int>, context: CPU.CPUContext) {
+        val value = context.registerMap[nibbles["src"]]?.value
+        val baseTenValue = value!!.toString(radix = 10)
+        val digits = baseTenValue.toString().map { it.digitToInt() }
+        context.ram.write(context.registers.A, digits[0].toUByte())
+        context.ram.write((context.registers.A + 1u).toUShort(), digits[1].toUByte())
+        context.ram.write((context.registers.A + 2u).toUShort(), digits[2].toUByte())
     }
 
-    override fun programCounter() {
-        TODO("Not yet implemented")
+    override fun programCounter(context: CPU.CPUContext) {
+        context.registers.P = (context.registers.P + 2u).toUShort()
     }
 }
