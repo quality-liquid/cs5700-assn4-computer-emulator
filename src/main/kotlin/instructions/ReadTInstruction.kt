a11f4227fc6ac7309fa40feedf3298e60c0d6f4d
Benjamin <hickenlooperbe@gmail.com>
2025-08-03 18:54:21 -0600

    implemented set T and read T instructions

--- src/main/kotlin/instructions/ReadTInstruction.kt
+++ src/main/kotlin/instructions/ReadTInstruction.kt
@@ -1,15 +1,17 @@
 package instructions
 
 class ReadTInstruction: InstructionTemplate {
-    override fun parseNibbles(instruction: Int) {
-        TODO("Not yet implemented")
+    override fun parseNibbles(instruction: Int): Map<String, Int> {
+        val nibbles = mutableMapOf<String, Int>()
+        nibbles["dest"] = (instruction shr 8) and 0x0f
+        return nibbles
     }
 
-    override fun performOperation() {
-        TODO("Not yet implemented")
+    override fun performOperation(nibbles: Map<String, Int>, context: CPU.CPUContext) {
+        context.registerMap[nibbles["dest"]]?.value = context.registers.T.value
     }
 
-    override fun programCounter() {
-        TODO("Not yet implemented")
+    override fun programCounter(context: CPU.CPUContext) {
+        context.registers.P = (context.registers.P + 2u).toUShort()
     }
 }
--- src/main/kotlin/instructions/SetTInstruction.kt
+++ src/main/kotlin/instructions/SetTInstruction.kt
@@ -1,15 +1,17 @@
 package instructions
 
 class SetTInstruction: InstructionTemplate {
-    override fun parseNibbles(instruction: Int) {
-        TODO("Not yet implemented")
+    override fun parseNibbles(instruction: Int): Map<String, Int> {
+        val nibbles = mutableMapOf<String, Int>()
+        nibbles["addr"] = (instruction and 0xff0) shr 4
+        return nibbles
     }
 
-    override fun performOperation() {
-        TODO("Not yet implemented")
+    override fun performOperation(nibbles: Map<String, Int>, context: CPU.CPUContext) {
+        context.registers.T.value = nibbles["addr"]!!.toUByte()
     }
 
-    override fun programCounter() {
-        TODO("Not yet implemented")
+    override fun programCounter(context: CPU.CPUContext) {
+        context.registers.P = (context.registers.P + 2u).toUShort()
     }
 }
