472eb1563b424ad23abd8dd572afe03ff465f2af
Benjamin <hickenlooperbe@gmail.com>
2025-08-03 18:20:22 -0600

    implemented the jump instruction

--- src/main/kotlin/instructions/JumpInstruction.kt
+++ src/main/kotlin/instructions/JumpInstruction.kt
@@ -1,15 +1,21 @@
 package instructions
 
 class JumpInstruction: InstructionTemplate {
-    override fun parseNibbles(instruction: Int) {
-        TODO("Not yet implemented")
+    override fun parseNibbles(instruction: Int): Map<String, Int> {
+        val nibbles = mutableMapOf<String, Int>()
+        nibbles["addr"] = instruction and 0xfff
+        return nibbles
     }
 
-    override fun performOperation() {
-        TODO("Not yet implemented")
+    override fun performOperation(nibbles: Map<String, Int>, context: CPU.CPUContext) {
+        val addr: Int = nibbles["addr"]!!
+        if ((addr % 2) != 0) {
+            throw IllegalArgumentException("program counter must be divisible by 2")
+        }
+        context.registers.P = addr.toUShort()
     }
 
-    override fun programCounter() {
-        TODO("Not yet implemented")
+    override fun programCounter(context: CPU.CPUContext) {
+        // no operation
     }
 }
