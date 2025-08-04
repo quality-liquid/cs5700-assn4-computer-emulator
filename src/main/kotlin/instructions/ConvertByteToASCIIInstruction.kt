318f178a75a6fcf86be6235ecceb6def651bb51f
Benjamin <hickenlooperbe@gmail.com>
2025-08-03 20:04:21 -0600

    implemented convert to ascii instruction

--- src/main/kotlin/instructions/ConvertByteToASCIIInstruction.kt
+++ src/main/kotlin/instructions/ConvertByteToASCIIInstruction.kt
@@ -1,15 +1,23 @@
 package instructions
 
 class ConvertByteToASCIIInstruction: InstructionTemplate {
-    override fun parseNibbles(instruction: Int) {
-        TODO("Not yet implemented")
+    override fun parseNibbles(instruction: Int): Map<String, Int> {
+        val nibbles = mutableMapOf<String, Int>()
+        nibbles["src"] = (instruction shr 8) and 0x0f
+        nibbles["dest"] = (instruction and 0xf0) shr 4
+        return nibbles
     }
 
-    override fun performOperation() {
-        TODO("Not yet implemented")
+    override fun performOperation(nibbles: Map<String, Int>, context: CPU.CPUContext) {
+        val value = context.registerMap[nibbles["src"]]!!.value
+        if (value > 0xfu) {
+            throw IllegalArgumentException("Can't copy value greater than 0xF as ASCII")
+        }
+        val valueInHex: String = value.toString(radix = 16)
+        context.registerMap[nibbles["dest"]]?.value = valueInHex.toCharArray().last().code.toUByte()
     }
 
-    override fun programCounter() {
-        TODO("Not yet implemented")
+    override fun programCounter(context: CPU.CPUContext) {
+        context.registers.P = (context.registers.P + 2u).toUShort()
     }
 }
